8566e73284dbe011ffcfeaf7315c8b336a882626
supermanshuvo <shuvothesuperman@gmail.com>
2021-10-22 10:11:28 +0600

    Project creat and deploy done

--- src/Login.js
+++ src/Login.js
@@ -3,30 +3,28 @@ import { Form, Button, Card, Alert } from "react-bootstrap"
 import { Link,useHistory } from "react-router-dom";
 import { useAuth } from "./contexts/AuthContext"
 
-export default function Signup() {
+
+
+export default function Login() {
   const emailRef = useRef()
   const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
-  const { signup } = useAuth()
+  const { login } = useAuth()
   const [error,setError] = useState("")
   const [loading,setLoading]=useState(false)
   const history = useHistory()
 
+
   async function handleSubmit(e) {
     e.preventDefault()
-
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match")
-    }
 
     try {
       setError("")
       setLoading(true)
-      await signup(emailRef.current.value, passwordRef.current.value)
-      history.push('/')
+      await login(emailRef.current.value, passwordRef.current.value)
+      history.push("/")
     } catch (err){
         console.log(err)
-      setError("Enter Password more then six and your email maybe used")
+      setError("Fail to Login")
     }
 
     setLoading(false)
@@ -36,7 +34,7 @@ export default function Signup() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">Log in</h2>
           { error && <Alert variant="danger">{error}</Alert> }
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
@@ -47,18 +45,17 @@ export default function Signup() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
             <Button disabled={loading} className="w-100 mt-3" type="submit">
-              Sign Up
+              Login
             </Button>
           </Form>
+          <div className="w-100 text-center mt-3">
+              <Link to="/forgot-password">Forget Password?</Link>
+          </div>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
+        Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
   )
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -8,25 +8,45 @@ export function useAuth(){
 }
 export function AuthProvider({children}){
     const [currentUser,setCurrentUser] = useState();
-    const [Loading,setLoading] = useState()
+    const [loading,setLoading]= useState(true)
     function signup(email,password){
        return auth.createUserWithEmailAndPassword(email,password);
     }
-
+    function login(email,password){
+        return auth.signInWithEmailAndPassword(email,password)
+    }
+    function logout(){
+        return auth.signOut()
+    }
+    function resetPassword(email){
+        return auth.sendPasswordResetEmail(email)
+    } 
+    function updateEmail(email) {
+        return currentUser.updateEmail(email)
+    }
+    
+    function updatePassword(password) {
+        return currentUser.updatePassword(password)
+    }
     useEffect(()=>{
         const unsubscribe = auth.onAuthStateChanged(user => {
-            setLoading(false)
             setCurrentUser(user)
+            setLoading(false)
           })
           return unsubscribe;
     },[])
     const  value={
         currentUser,
-        signup
+        signup,
+        login,
+        logout,
+        resetPassword,
+        updateEmail,
+        updatePassword
     }
     return(
         <AuthContext.Provider value={value}>
-            {Loading && children}
+            { !loading && children}
         </AuthContext.Provider>
     );
 }
